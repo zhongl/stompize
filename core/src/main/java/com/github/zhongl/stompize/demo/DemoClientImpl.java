284797169d0b750db5de084e249991085648a834
zhongl <zhong.lunfu@gmail.com>
2013-02-26 10:38:15 +0800

    Clean code.

--- core/src/main/java/com/github/zhongl/stompize/demo/DemoClientImpl.java
+++ core/src/main/java/com/github/zhongl/stompize/demo/DemoClientImpl.java
@@ -17,11 +17,11 @@ import static com.github.zhongl.stompize.Bytes.buf;
 public class DemoClientImpl extends DemoClient {
 
     private static final ByteBuf SEND        = buf("SEND");
-    private static final ByteBuf DESTINATION = buf("\ndestination:");
-    private static final ByteBuf TRANSACTION = buf("\ntransaction:");
-    private static final ByteBuf RECEIPT     = buf("\nreceipt:");
     private static final ByteBuf ACK         = buf("ACK");
-    private static final ByteBuf ID          = buf("\nid:");
+    private static final ByteBuf destination = buf("\ndestination:");
+    private static final ByteBuf transaction = buf("\ntransaction:");
+    private static final ByteBuf receipt     = buf("\nreceipt:");
+    private static final ByteBuf id          = buf("\nid:");
 
     private final Channel channel;
 
@@ -40,9 +40,9 @@ public class DemoClientImpl extends DemoClient {
         List<ByteBuf> components = new ArrayList<ByteBuf>();
         components.add(SEND);
 
-        Stompize.addRequiredHeaderTo(components, DESTINATION, destination);
-        Stompize.addOptionalHeaderTo(components, TRANSACTION, transaction);
-        Stompize.addOptionalHeaderTo(components, RECEIPT, receipt);
+        Stompize.addRequiredHeaderTo(components, DemoClientImpl.destination, destination);
+        Stompize.addOptionalHeaderTo(components, DemoClientImpl.transaction, transaction);
+        Stompize.addOptionalHeaderTo(components, DemoClientImpl.receipt, receipt);
         Stompize.addContentTo(components, content);
 
         channel.write(new DefaultCompositeByteBuf(UnpooledByteBufAllocator.HEAP_BY_DEFAULT, false, 32, components));
@@ -54,9 +54,9 @@ public class DemoClientImpl extends DemoClient {
         List<ByteBuf> components = new ArrayList<ByteBuf>();
         components.add(ACK);
 
-        Stompize.addRequiredHeaderTo(components, ID, id);
-        Stompize.addOptionalHeaderTo(components, RECEIPT, receipt);
-        Stompize.addOptionalHeaderTo(components, TRANSACTION, transaction);
+        Stompize.addRequiredHeaderTo(components, DemoClientImpl.id, id);
+        Stompize.addOptionalHeaderTo(components, DemoClientImpl.receipt, receipt);
+        Stompize.addOptionalHeaderTo(components, DemoClientImpl.transaction, transaction);
         Stompize.addContentTo(components, null);
 
         channel.write(new DefaultCompositeByteBuf(UnpooledByteBufAllocator.HEAP_BY_DEFAULT, false, 32, components));
